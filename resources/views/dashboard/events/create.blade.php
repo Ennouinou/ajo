8cdc3c8609336070687d18bd5840de47203e2e93
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-07-26 00:29:53 +0100

    simple modification

--- resources/views/dashboard/events/create.blade.php
+++ resources/views/dashboard/events/create.blade.php
@@ -7,23 +7,24 @@
             <form method="POST" action="{{route('dashboard.posts.store')}}" enctype="multipart/form-data">
                 @csrf
                 @include('dashboard.events.form')
-                <div class="my-fieldset mt-4 bgwo">
+                <div class="my-fieldset bgwo mt-4">
                     <legend class="purple">Atachements :</legend>
-                    <div class="form-group col-md-6">
-                        <label for="videos" >
-                            <span class="blue"> Videos :</span> <a class="btn btn-primary mt-1 bg-blue" onclick="createVideoInput()" ><i class="fa fa-plus "></i></a>
-                        </label>
-                        <span id="videos">
-                        </span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="attachments" >
-                           <span class="blue">Images :</span> <a class="btn btn-primary mt-1 bg-blue" onclick="createImageInput()"><i class="fa fa-plus"></i></a>
-                        </label>
-                        <span id="images">
-                        </span>
+                    <div class="form-group d-flex">
+                        <div class="col-md-6">
+                            <label for="videos" >
+                                <span class="blue"> Videos :</span> <a class="btn btn-primary mt-1 bg-blue" onclick="createVideoInput()" ><i class="fa fa-plus "></i></a>
+                            </label>
+                            <span id="videos"></span>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="attachments" >
+                                <span class="blue">Images :</span> <a class="btn btn-primary mt-1 bg-blue" onclick="createImageInput()"><i class="fa fa-plus"></i></a>
+                            </label>
+                            <span id="images"></span>
+                        </div>
                     </div>
                 </div>
+
 
                 @if ($errors->any())
                     <ul>
