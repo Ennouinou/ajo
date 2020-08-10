9a370e8ebf97c2bb55876057da67871fdde2e0ff
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-08-10 15:34:12 +0100

    register https

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -7,8 +7,8 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Administration</title>
-    <link href="{{URL::to('/')}}/dashboard/css/styles.css" rel="stylesheet" />
-    <link href="{{URL::to('/')}}/assets/app/css/main.css" rel="stylesheet" />
+    <link href="/dashboard/css/styles.css" rel="stylesheet" />
+    <link href="/assets/app/css/main.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
 </head>
@@ -27,13 +27,13 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="{{URL::to('/')}}/dashboard/js/scripts.js"></script>
+    <script src="/dashboard/js/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="{{URL::to('/')}}/dashboard/assets/demo/chart-area-demo.js"></script>
-    <script src="{{URL::to('/')}}/dashboard/assets/demo/chart-bar-demo.js"></script>
+    <script src="/dashboard/assets/demo/chart-area-demo.js"></script>
+    <script src="/dashboard/assets/demo/chart-bar-demo.js"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="{{URL::to('/')}}/dashboard/assets/demo/datatables-demo.js"></script>
-    <script src="{{URL::to('/')}}/assets/app/js/main.js"></script>
+    <script src="/dashboard/assets/demo/datatables-demo.js"></script>
+    <script src="/assets/app/js/main.js"></script>
 </body>
 </html>
