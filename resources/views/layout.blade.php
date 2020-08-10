144d4627c69966d5632411c35334e4cc5be3221f
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-08-10 14:58:07 +0100

    https test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,18 +8,18 @@
 
     <title>AJO</title>
 
-    <link rel="shortcut icon" href="{{URL::to('/public')}}/assets/images/ajo-favicon.png">
+    <link rel="shortcut icon" href="/assets/images/ajo-favicon.png">
     <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-    <link rel="stylesheet" href="{{URL::to('/')}}/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{URL::to('/')}}/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
     <!-- Custom styles for our template -->
-    <link rel="stylesheet" href="{{URL::to('/')}}/assets/css/bootstrap-theme.css" media="screen" >
-    <link rel="stylesheet" href="{{URL::to('/')}}/assets/css/main.css">
-    <link rel="stylesheet" href="{{URL::to('/')}}/assets/app/css/main.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap-theme.css" media="screen" >
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/app/css/main.css">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9] -->
-    <script src="{{URL::to('/')}}/assets/js/html5shiv.js"></script>
-    <script src="{{URL::to('/')}}/assets/js/respond.min.js"></script>
+    <script src="/assets/js/html5shiv.js"></script>
+    <script src="/assets/js/respond.min.js"></script>
     <![endif]-->
 </head>
 <body class="home" onload="menuChange()">
